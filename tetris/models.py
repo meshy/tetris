5f6fcfe5c2d2da0568b4d4c0e0934902c348e0e9
stuartmck <stuart_mckenzie@btinternet.com>
2014-06-15 19:26:39 +0100

    Created classes for each piece

--- tetris/models.py
+++ tetris/models.py
@@ -7,9 +7,108 @@ class State(Enum):
     moving = 2
 
 
+class Translation(Enum):
+    left = (-1, 0)
+    right = (1, 0)
+    down = (0, 1)
+
+
+class Rotation(Enum):
+    left = -1
+    right = 1
+
+
 class Block:
     def __init__(self, state=State.empty):
         self.state = state
+
+
+class Piece:
+    def move(self, translation):
+        transform = translation.value
+        for orientation in self.orientations:
+            for coordinate in orientation:
+                coordinate[0] += transform[0]
+                coordinate[1] += transform[1]
+
+    def get_coords(self):
+        return self.orientations[self.rotation]
+
+    def rotate(self, rotation):
+        limit = len(self.orientations) + 1
+        self.rotation +=  rotation.value + limit
+        self.rotation %= limit
+
+
+class TPiece(Piece):
+    def __init__(self, midpoint):
+        self.rotation = 0
+        self.orientations = [
+            [[midpoint, 0], [midpoint + 1, 0], [midpoint + 2, 0], [midpoint, 1]],
+            [[midpoint, 0], [midpoint, 1], [midpoint, 2], [midpoint + 1, 1]],
+            [[midpoint, 0], [midpoint - 1, 1], [midpoint, 1], [midpoint + 1, 1]],
+            [[midpoint, 0], [midpoint - 1, 1], [midpoint, 1], [midpoint, 2]],
+        ]
+
+
+class SquarePiece(Piece):
+    def __init__(self, midpoint):
+        self.rotation = 0
+        self.orientations = [
+            [[midpoint, 0], [midpoint + 1, 0], [midpoint + 2, 0], [midpoint, 1]],
+        ]
+
+    def rotate(self, direction):
+        pass
+
+
+class LLPiece(Piece):
+    def __init__(self, midpoint):
+        self.rotation = 0
+        self.orientations = [
+            [[midpoint, 0], [midpoint + 1, 0], [midpoint + 2, 0], [midpoint + 2, 1]],
+            [[midpoint, 2], [midpoint + 1, 0], [midpoint + 1, 1], [midpoint + 1, 2]],
+            [[midpoint, 0], [midpoint, 1], [midpoint + 1, 1], [midpoint + 2, 1]],
+            [[midpoint, 0], [midpoint + 1, 0], [midpoint, 1], [midpoint, 2]],
+        ]
+
+
+class RLPiece(Piece):
+    def __init__(self, midpoint):
+        self.rotation = 0
+        self.orientations = [
+            [[midpoint, 0], [midpoint + 1, 0], [midpoint + 2, 0], [midpoint, 1]],
+            [[midpoint, 0], [midpoint + 1, 0], [midpoint + 1, 1], [midpoint + 1, 2]],
+            [[midpoint + 2, 0], [midpoint, 1], [midpoint + 1, 1], [midpoint + 2, 1]],
+            [[midpoint, 0], [midpoint, 1], [midpoint, 2], [midpoint + 1, 2]],
+        ]
+
+
+class BarPiece(Piece):
+    def __init__(self, midpoint):
+        self.rotation = 0
+        self.orientations = [
+            [[midpoint - 1, 1], [midpoint , 1], [midpoint + 1, 1], [midpoint + 2, 1]],
+            [[midpoint, 0], [midpoint, 1], [midpoint, 2], [midpoint, 3]],
+        ]
+
+
+class LSPiece(Piece):
+    def __init__(self, midpoint):
+        self.rotation = 0
+        self.orientations = [
+            [[midpoint, 0], [midpoint + 1, 0], [midpoint + 1, 1], [midpoint + 2, 1]],
+            [[midpoint + 2, 0], [midpoint + 1, 1], [midpoint + 2, 1], [midpoint + 1, 2]],
+        ]
+
+
+class RSPiece(Piece):
+    def __init__(self, midpoint):
+        self.rotation = 0
+        self.orientations = [
+            [[midpoint + 1, 0], [midpoint + 2, 0], [midpoint, 1], [midpoint + 1, 1]],
+            [[midpoint, 0], [midpoint, 1], [midpoint + 1, 1], [midpoint + 1, 2]],
+        ]
 
 
 class Grid:
